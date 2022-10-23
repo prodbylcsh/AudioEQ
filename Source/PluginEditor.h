687363fccc953e3494db89006c147d64525d1be6
Lukáš Schöbel <seblis@seznam.cz>
2022-10-23 15:47:03 +0200

    Implemented Analyzer Enablement button

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -224,6 +224,11 @@ struct ResponseCurveComponent : juce::Component,
 
     void paint(juce::Graphics& g) override;
     void resized() override;
+
+    void toggleAnalysisEnablement(bool enabled)
+    {
+        shouldShowFFTAnalysis = enabled;
+    }
 private:
     AudioEQAudioProcessor& audioProcessor;
     juce::Atomic<bool> parametersChanged{ false };
@@ -239,6 +244,8 @@ private:
     juce::Rectangle<int> getAnalysisArea();
 
     PathProducer leftPathProducer, rightPathProducer;
+
+    bool shouldShowFFTAnalysis = true;
 };
 
 struct PowerButton : juce::ToggleButton { };
